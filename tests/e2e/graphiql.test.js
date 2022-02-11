0bc2e4583dced8048f5a9feeed9f6090cddcca7b
chriszarate <chris.zarate@automattic.com>
2022-02-11 14:30:21 -0500

    ?

--- tests/e2e/graphiql.test.js
+++ tests/e2e/graphiql.test.js
@@ -11,19 +11,19 @@ import { loginUser } from '@wordpress/e2e-test-utils/build/login-user'
 describe('Graphiql', function () {
 
     it ('can open graphiql admin page', async () => {
-        await visitAdminPage( 'admin.php', 'page=graphiql-ide' );
+        await visitAdminPage( 'admin.php', '?page=graphiql-ide' );
         await wait( 1000 );
         expect( true ).toBeTruthy();
     })
 
     it ('can open graphql settings page', async () => {
-        await visitAdminPage( 'admin.php', 'page=graphql_settings' );
+        await visitAdminPage( 'admin.php', '?page=graphql_settings' );
         await wait( 1000 );
         expect( true ).toBeTruthy();
     })
 
     it ('can open test admin page', async () => {
-        await visitAdminPage( 'admin.php', 'page=tester-page' );
+        await visitAdminPage( 'admin.php', '?page=tester-page' );
         await wait( 1000 );
         expect( true ).toBeTruthy();
     })
